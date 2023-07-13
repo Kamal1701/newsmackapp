7b1003d8550ea5e28d11d2caad19645ba4f8a3a8
Kamal1701 <kk17kannan@yahoo.com>
2023-07-13 09:38:27 +0530

    New Smack App initial commit

---
+++ app/src/main/java/com/example/newsmackapp/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.newsmackapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+    fun onLoginLoginBtnClicked(view : View){
+
+    }
+
+    fun onLoginCreateUserBtnClicked(view: View){
+
+    }
+}
